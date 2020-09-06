4826f4c296f98f3b5e1909f33ce7a2a9896c403d
Priliv <stadnikd53@gmail.com>
2020-09-06 15:45:47 +0300

    first lesson

---
+++ src/com/company/Main.java
@@ -0,0 +1,32 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        float f = 3.14f;
+        int in = 1;
+        int in2;
+        in2 = 3;
+        char c = 'c';
+                long l = 1234567890l;
+                byte b = 122;
+                boolean bb = true;
+                short s = 1323;
+                String ss = "fg";
+                int sum = in + in2;
+                int diff = in - in2;
+                int mult = in * in2;
+                double div = in / in2;
+        System.out.println(sum);
+        System.out.println(diff);
+
+        System.out.println(mult);
+        System.out.println(div);
+
+
+        int tt = 2004 + 11 + 24;
+
+        System.out.println(tt);
+    }
+}
